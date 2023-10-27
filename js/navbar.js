a7b08f468b74d97adfc17a5c31bffd8ac55543ba
Sai Vishnu <saivishnu725@gmail.com>
2023-10-27 22:27:40 +0530

    kinda works. Will make the bottom div look better

--- js/navbar.js
+++ js/navbar.js
@@ -2,10 +2,12 @@ const blob = document.getElementById("blob");
 
 window.onpointermove = event => {
     console.log("event ");
-  const { clientX, clientY } = event;
-  console.log(clientX + " " + clientY);
-  blob.animate({
-    left: `${clientX}px`,
-    top: `${clientY}px`
-  }, { duration: 3000, fill: "forwards" });
+    const { clientX, clientY } = event;
+    console.log(clientX + " " + clientY);
+    if (window.scrollY <= window.innerHeight) {
+        blob.animate({
+            left: `${clientX}px`,
+            top: `${clientY}px`
+        }, { duration: 3000, fill: "forwards" });
+    }
 }
