f4ce4f975fbbe98583c4e2e0e0867e1a948c0b4f
Sai Vishnu <saivishnu725@gmail.com>
2023-10-28 12:35:54 +0530

    works yayyy

---
+++ public/js/hacker-ish.js
@@ -0,0 +1,40 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+let next = 0;
+
+document.querySelector("#hack").onclick = (event) => {
+  let iteration = 0;
+  let words = ["SAI VISHNU", "THE UNCONCERNED APE"];
+
+  interval = setInterval(() => {
+    if (next == 1) {
+      event.target.style.fontSize = "4rem";
+    } else {
+      event.target.style.fontSize = "5rem";
+    }
+    event.target.innerText = words[next]
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return words[next][index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= words[next].length) {
+      clearInterval(interval);
+      if (event.target.innerText === words[0]) {
+        next = 1;
+      } else if (event.target.innerText === words[1]) {
+        next = 0;
+      }
+
+      document.querySelector("#hack").setAttribute("data-value", words[next]);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+};
