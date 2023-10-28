3c30e6fd9d74b73ccd0e97875c08f3bc16100402
Sai Vishnu <saivishnu725@gmail.com>
2023-10-28 12:33:09 +0530

    removed home-grown debugging

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,9 +1,7 @@
 const blob = document.getElementById("blob");
 
 window.onpointermove = event => {
-    console.log("event ");
     const { clientX, clientY } = event;
-    console.log(clientX + " " + clientY);
     if (window.scrollY <= window.innerHeight) {
         blob.animate({
             left: `${clientX}px`,
